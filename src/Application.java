ae7271b291a5aaa7abc9a0cccd5da2c1b38f4b56
Somveer Dahiya <somveerdahiya009@gmail.com>
2026-02-03 16:05:20 +0000

    Revert "Update Reservation Service commit"
    This reverts commit 0a781eaa5ccb669777467aa6b32f95926fb5e604.

--- src/Application.java
+++ src/Application.java
@@ -1,4 +1,3 @@
-import services.UpdateReservation;
 import services.ViewReservation;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -12,8 +11,8 @@ public class Application {
         String password = "Dahiya@2005";
         Scanner sc = new Scanner(System.in);
         try(Connection conn = DriverManager.getConnection(url,userName,password)) {
-            UpdateReservation u = new UpdateReservation();
-            u.updateReservation(conn,sc);
+            ViewReservation v = new ViewReservation() ;
+            v.viewReservation(conn,sc);
         }
         catch (SQLException e){
             System.out.println("Connection Failed -->"+e.getMessage());
